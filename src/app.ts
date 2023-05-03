59bbd872c1c46fc3424715ae72ddba410e1f84cc
Max <maxime.leb@gmail.com>
2023-05-03 11:59:42 +0200

    fix API

--- src/app.ts
+++ src/app.ts
@@ -36,13 +36,13 @@ form.addEventListener("submit", (e: SubmitEvent) => {
     setLoadingItems()
 
     //fetch API
-    const api_key = import.meta.env.VITE_API_KEY
+    const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY
 
     fetch(`https://api.openai.com/v1/completions`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${api_key}`
+            Authorization: `Bearer ${OPENAI_API_KEY}`
         },
         body: JSON.stringify({
             prompt: generatePromptByCourseAndIngredients(
